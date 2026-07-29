9d1eda2b8c2930a90b4cbdedbd2556df07fc39b9
JumershO1 <www.junaid555fakir@gmail.com>
2026-07-29 23:59:30 +0530

    somechange

--- BootMVCProj10_Interceptors_2/src/main/java/com/nt/controller/IntercepotrExampleController.java
+++ BootMVCProj10_Interceptors_2/src/main/java/com/nt/controller/IntercepotrExampleController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class IntercepotrExampleController {
-
+//some change
 	@GetMapping("/")
 	public String home() {
 		return "Welcome to home";
